e3d8f6a425b9bbf77be47ca8f02359bbe3b8c40b
KyanBirb <97422254+KyanBirb@users.noreply.github.com>
2026-06-15 17:08:05 -0500

    Fix f5 camera colliding with leaves

--- common/src/main/java/com/farcr/treephysics/mixin/walk_through_leaves/BlockStateBaseMixin.java
+++ common/src/main/java/com/farcr/treephysics/mixin/walk_through_leaves/BlockStateBaseMixin.java
@@ -4,10 +4,14 @@ import com.farcr.treephysics.api.util.TreeUtil;
 import com.farcr.treephysics.index.TreePhysicsConfig;
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
@@ -33,6 +37,15 @@ public abstract class BlockStateBaseMixin {
         }
 
         return original.call(level, pos);
+    }
+
+    @WrapMethod(method = "getVisualShape")
+    private VoxelShape treephysics$getVisualShape(BlockGetter level, BlockPos pos, CollisionContext context, Operation<VoxelShape> original) {
+        if(TreeUtil.isLeaf(this.asState()) && TreePhysicsConfig.LEAF_WALKING_BEHAVIOR.get() != TreePhysicsConfig.LeafWalkingBehavior.NEVER) {
+            return Shapes.empty();
+        }
+
+        return original.call(level, pos, context);
     }
 
 }
--- common/src/main/java/com/farcr/treephysics/mixin/walk_through_leaves/CanFallAtLeastHelperMixin.java
+++ common/src/main/java/com/farcr/treephysics/mixin/walk_through_leaves/CanFallAtLeastHelperMixin.java
@@ -1,12 +1,12 @@
 package com.farcr.treephysics.mixin.walk_through_leaves;
 
+import com.farcr.treephysics.api.util.TreeUtil;
 import com.farcr.treephysics.index.TreePhysicsConfig;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.ryanhcode.sable.mixinhelpers.CanFallAtleastHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -18,7 +18,7 @@ public class CanFallAtLeastHelperMixin {
 
     @WrapOperation(method = "canFallAtleastWithSubLevels", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/phys/shapes/VoxelShape;"))
     private static VoxelShape treephysics$getCollisionShape(BlockState instance, BlockGetter blockGetter, BlockPos pos, Operation<VoxelShape> original) {
-        if(instance.getBlock() instanceof LeavesBlock && TreePhysicsConfig.LEAF_WALKING_BEHAVIOR.get().allowSubLevel()) {
+        if(TreeUtil.isLeaf(instance) && TreePhysicsConfig.LEAF_WALKING_BEHAVIOR.get().allowSubLevel()) {
             return Shapes.empty();
         }
         return original.call(instance, blockGetter, pos);
